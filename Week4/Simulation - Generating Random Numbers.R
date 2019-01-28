ecfb4f2db4b329526d513905388185283162f554
Igot Tavares <iv_tavares@yahoo.com.br>
2019-01-28 00:20:40 -0200

    Quiz 4 (Complete)

---
+++ Week4/Simulation - Generating Random Numbers.R
@@ -0,0 +1,17 @@
+x <-rnorm(10)
+x
+x <- rnorm(10, 20, 2)
+x
+summary(x)
+set.seed(1)
+rnorm(5)
+rnorm(5)
+set.seed(1)
+rnorm(5)
+rpois(10, 1)
+rpois(10, 2)
+rpois(10, 20)
+
+ppois(2, 2)
+ppois(4, 2)
+ppois(6, 2)
